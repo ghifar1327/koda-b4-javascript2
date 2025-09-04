677a0d1aa65f59232bc7fae99c4eae883787072a
ghifar1327 <givartorreto@gmail.com>
2025-09-04 14:05:29 +0700

    Add files via upload

--- minitask4.js
+++ minitask4.js
@@ -1,18 +1,20 @@
-
-let suhu = 34;
+let suhu = 1;
 // keyword function
 function cF(suhu) {
   return suhu * 1.18 + " fahrenheit";
 }
 // Anonymous function
 const cR = function (suhu) {
-  return (4 / 5) * suhu + " reamur";
+  return (4 / 5) * suhu;
 };
 // arrow function
 const cK = (suhu) => {
-  return suhu + 273.15 + " kelvin";
+  return suhu + 273.15;
 };
 
-console.log(cF(suhu));
-console.log(cR(suhu));
-console.log(cK(suhu));
+function informasi() {
+  return `suhu dalam celcius adalah ${suhu} C, dalam fahrenheit adalah ${cF(
+    suhu
+  )}, dalam reamur adalah ${cR(suhu)}, dan dalam kelvin adalah ${cK(suhu)}`;
+}
+console.log(informasi());
