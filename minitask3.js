03e20dc75bea9b310849474d103ad5372fa88e5a
ghifar1327 <givartorreto@gmail.com>
2025-09-04 15:56:46 +0700

    Add files via upload

--- minitask3.js
+++ minitask3.js
@@ -1,53 +1,57 @@
-// no 1
-let tinggiPersegi = 5;
+const obj = {
+  str: [
+    0,
+    1,
+    2,
+    [
+      0,
+      [
+        0,
+        1,
+        {
+          man: [
+            {
+              the: {
+                name: {
+                  of: {
+                    us: "koda",
+                  },
+                },
+              },
+            },
+          ],
+        },
+      ],
+    ],
+  ],
+};
 
-for (let i = 1; i <= tinggiPersegi; i++) {
-  let baris = "";
-  for (let j = 1; j <= tinggiPersegi; j++) {
-    if (i === 1 || i === tinggiPersegi) {
-      baris += "*1";
-    } else {
-      if (j === 1 || j === tinggiPersegi) {
-        baris += "*2";
-      } else {
-        baris += "  ";
-      }
-    }
-  }
-  console.log(baris);
-}
-// no 2
-let n = 5;
-for (i = 1; i <= n; i++) {
-  let output = "";
-  for (j = i; j <= n; j++) {
-    output += " ";
-  }
-  for (k = 1; k <= i; k++) {
-    output += "* ";
-  }
-  console.log(output);
-}
-// no 3
-let t = 5;
-for (x = 0; x < t; x++) {
-  let btg2 = "";
-  for (y = 0; y < 11; y++) {
-    if (x == 0 || x == 2 || x === 4) {
-      if (y % 2 === 0) {
-        btg2 += "*";
-      } else {
-        btg2 += " ";
-      }
-    } else {
-      if (x == 1 && y == 8) {
-        btg2 += "*";
-      } else if (y != 10) {
-        btg2 += " ";
-      } else {
-        btg2 += "*";
-      }
-    }
-  }
-  console.log(btg2);
-}
+const {
+  str: [
+    _,
+    __,
+    ___,
+    [
+      ____,
+      [
+        _____,
+        ______,
+        {
+          man: [
+            {
+              the: {
+                name: {
+                  of: { us },
+                },
+              },
+            },
+          ],
+        },
+      ],
+    ],
+  ],
+} = obj;
+console.log(us);
+
+// console.log(obj.str[3][1][2].man[0].the.name.of.us);
+// 'koda'
